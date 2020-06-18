9e5d0ac6a8ee6b7f6f6cff917aa2ab270a1388b4
Søren Pedersen <sh.pedersen@gmail.com>
2020-06-18 18:10:43 +0200

    Add initial implementation

---
+++ AzureStorageQueueWorker.cs
@@ -0,0 +1,66 @@
+﻿namespace Likvido.Worker.AzureStorageQueue
+{
+    using System;
+    using System.Linq;
+    using System.Text.Json;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Azure.Storage.Queues;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public abstract class AzureStorageQueueWorker<T> : BackgroundService
+    {
+        private readonly ILogger logger;
+        private readonly string azureStorageConnectionString;
+        private readonly string queueName;
+        private readonly TimeSpan? visibilityTimeout;
+
+        public AzureStorageQueueWorker(
+            ILogger logger,
+            string azureStorageConnectionString,
+            string queueName,
+            TimeSpan? visibilityTimeout = null)
+        {
+            this.logger = logger;
+            this.azureStorageConnectionString = azureStorageConnectionString;
+            this.queueName = queueName;
+            this.visibilityTimeout = visibilityTimeout;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation("Starting...");
+
+            var queueClient = new QueueClient(azureStorageConnectionString, queueName);
+            await queueClient.CreateIfNotExistsAsync();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var queueMessageResponse = await queueClient.ReceiveMessagesAsync(1, visibilityTimeout, stoppingToken);
+                    var queueMessage = queueMessageResponse.Value.FirstOrDefault();
+
+                    if (queueMessage == null)
+                    {
+                        logger.LogInformation("No messages, sleeping...");
+                        await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                        continue;
+                    }
+
+                    var message = JsonSerializer.Deserialize<T>(queueMessage.MessageText);
+                    await ProcessMessage(message);
+                    await queueClient.DeleteMessageAsync(queueMessage.MessageId, queueMessage.PopReceipt, stoppingToken);
+                }
+                catch (TaskCanceledException) { }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error handling message");
+                }
+            }
+        }
+
+        protected abstract Task ProcessMessage(T message);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Likvido.Worker.AzureStorageQueue
-Likvido.Worker.AzureStorageQueue
+Small wrapper to ease creating services that read from Azure Storage Queues
+
+## Usage
+Create a new Worker service project, and install this package. Then change the base class of your `Worker` to `AzureStorageQueueWorker<T>` where `T` is the type of message you expect to receive (a POCO object):
+
+```
+public class Worker : AzureStorageQueueWorker<MyCustomMessage>
+{
+    private readonly ILogger<Worker> logger;
+
+    public Worker(
+        ILogger<Worker> logger,
+        IConfiguration configuration)
+        : base(logger, configuration["AZURE_STORAGE_CONNECTION_STRING"], configuration["QUEUE_NAME"])
+    {
+        this.logger = logger;
+    }
+
+    protected override async Task ProcessMessage(MyCustomMessage message)
+    {
+        // TODO: Process the message
+    }
+}
+```
