e4f6e76a7a00c2d0b3b7a2808a548019cb7b4f9b
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-10-16 12:21:06 +0700

    Updates nuget dependencies. Adds ability to add sampling

---
+++ src/AvoidSamplingTelemetryInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Likvido.Worker.AzureStorageQueue
+{
+    public class AvoidSamplingTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly Func<ITelemetry, bool> _condition;
+
+        public AvoidSamplingTelemetryInitializer(Func<ITelemetry, bool> condition)
+        {
+            _condition = condition ?? throw new ArgumentNullException(nameof(condition));
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            //https://stackoverflow.com/a/60951047/1643576
+            if (telemetry is ISupportSampling supportSampling
+                &&_condition(telemetry))
+            {
+                supportSampling.SamplingPercentage = 100;
+            }
+        }
+    }
+}
