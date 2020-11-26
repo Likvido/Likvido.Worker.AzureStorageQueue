bed12c6bb6e7e85f9592595e6b845814450f690c
Søren Pedersen <sh.pedersen@gmail.com>
2020-11-26 12:47:28 +0100

    Make Time and Data optional

--- src/MessageFormats/CloudEvent.cs
+++ src/MessageFormats/CloudEvent.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+
 namespace Likvido.Worker.AzureStorageQueue.MessageFormats
 {
-    public class CloudEvent<TMessage>
+    public class CloudEvent
     {
         public string Id { get; set; } = null!;
         public string Source { get; set; } = null!;
-        public TMessage Data { get; set; } = default!;
         public string Type { get; set; } = null!;
-        public DateTime Time { get; set; }
+        public DateTime? Time { get; set; }
         public string SpecVersion { get; set; } = null!;
+    }
+
+    public class CloudEvent<TMessage> : CloudEvent
+    {
+        public TMessage Data { get; set; } = default!;
     }
 }
