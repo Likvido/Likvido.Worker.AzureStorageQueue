28de487d23a33cd3f4844c2f7fca48eaf3fc14cb
Søren Pedersen <sh.pedersen@gmail.com>
2020-11-26 09:41:05 +0100

    Add skeleton for the generic message structure for cloud events

---
+++ src/MessageFormats/CloudEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Likvido.Worker.AzureStorageQueue.MessageFormats
+{
+    public class CloudEvent<TMessage>
+    {
+        public string Id { get; set; } = null!;
+        public string Source { get; set; } = null!;
+        public TMessage Data { get; set; } = default!;
+        public string Type { get; set; } = null!;
+        public DateTime Time { get; set; }
+        public string SpecVersion { get; set; } = null!;
+    }
+}
